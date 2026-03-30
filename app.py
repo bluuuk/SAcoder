d353d5a36eb8b6ad68d94e000f2a10b2051f6110
Lukas Boschanski <lukas.boschanski@student.hpi.de>
2026-03-30 19:18:31 -0400

    mongo db integration test

--- app.py
+++ app.py
@@ -230,9 +230,9 @@ else:
         st.success("No uncoded advice items remain for your alias.")
     else:
         st.markdown("### Advice Item")
-        advice_text = st.session_state.current_advice.advice
+        advice_text = st.session_state.current_advice
         if advice_text:
-            st.info(advice_text)
+            st.info(advice_text.advice)
         else:
             st.error("Loaded item, but no advice text field was found.")
 
