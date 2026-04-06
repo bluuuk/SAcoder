e3eaed0936039f0e8a8661435a870f1eac8f05e2
Lukas Boschanski <24961549+bluuuk@users.noreply.github.com>
2026-04-05 21:21:24 -0400

    remove q6 as we have no end user

--- coding.py
+++ coding.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-
 class CodingNode:
     def __init__(
         self,
@@ -43,7 +42,7 @@ CodingLabels = {
     "P3": "Infeasible Practice",
     "P4": "Security Expert",
     "P5": "IT Specialist",
-    "P6": "Average developer",
+    "P6": "End-user",
     "N": "Security Principle",
 }
 
@@ -62,18 +61,18 @@ Q8 = CodingNode(
     question_text="Is it intended that a security expert carry out this item?",
     help_text="Does following this advice require an expert understanding of security and security implementation in order to properly follow the advice? Someone following this advice item would have to be an expert in security to be able to understand it and successfully follow it, or be capable of extracting actionable steps from an otherwise non-actionable item based on their experience.",
 )
-Q7 = CodingNode(
-    label="Q7",
-    yes=CodingNode("P6"),
-    no=Q8,
-    question_text="Is it intended that the average developer carry out this item?",
-    help_text="Does the item suggest that the average developer can carry out this practice?",
-)
+# Q7 = CodingNode(
+#     label="Q7",
+#     yes=CodingNode("P6"),
+#     no=Q8,
+#     question_text="Is it intended that the end-user carry out this item?",
+#     help_text="Does the item suggest that the end-user can carry out this practice?",
+# )
 Q6 = CodingNode(
     label="Q6",
     no=CodingNode("P3"),
-    yes=Q7,
-    question_text="Is it viable to accomplish with reasonable resources for a developer?",
+    yes=Q8,
+    question_text="Is it viable to accomplish with reasonable resources?",
     help_text="Could the advice item be followed with an acceptable cost?. E.g., the advice would not take years to follow, or have cost out of line with the anticipated benefit.",
 )
 Q5 = CodingNode(
